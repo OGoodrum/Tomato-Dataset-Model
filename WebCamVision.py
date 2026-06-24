ca382efcf07e24907d9aa949ce0d98d8c74d6517
Oliver Goodrum <ogoodrum@uvic.ca>
2026-06-23 17:05:21 -0700

    Created the file WebCamVision.py that looks at the webcam and recognizes any tomato leaves and their class in real time.

---
+++ WebCamVision.py
@@ -0,0 +1,34 @@
+import cv2
+from ultralytics import YOLO
+
+
+model = YOLO('c:/Users/olive/OneDrive/Documents/Tomato Dataset/CVResults/content/runs/detect/train/weights/last.pt')  # or another version of YOLOv8 (e.g., yolov8s.pt for small)
+
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    ret, frame = cap.read()
+
+    results = model(frame)[0]
+
+    for result in results.boxes.data.tolist():  # Each detection in the format [x1, y1, x2, y2, conf, class]
+        x1, y1, x2, y2, conf, cls = result[:6]
+        label = f'{model.names[cls]} {conf:.2f}'
+        
+        # Draw bounding box and label on the frame
+        if conf > 0.5: 
+                cv2.rectangle(frame, (int(x1), int(y1)), (int(x2), int(y2)), (0, 0, 255), 4)  # Bounding box
+                cv2.putText(frame, label, (int(x1), int(y1) - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 2)
+
+    cv2.imshow('frame', frame)
+
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+    elif cv2.getWindowProperty('frame', cv2.WND_PROP_VISIBLE) < 1:
+        break
+
+cap.release()
+
+cv2.destroyAllWindows()
