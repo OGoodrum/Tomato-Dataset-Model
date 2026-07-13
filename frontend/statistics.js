dd820e2aaf5dde7bfbc99873147e02812d5fb3be
Oliver Goodrum <olivergoodrum.og@gmail.com>
2026-07-13 11:29:52 -0700

    Add some stats to the statistics page through functionality with statistics.js

---
+++ frontend/statistics.js
@@ -0,0 +1,29 @@
+import { createClient } from 'npm:@supabase/supabase-js@2'
+
+// Initialize the Supabase client
+const SUPABASE_URL = "https://vpofkrbxyaxvzryhmdte.supabase.co";
+const SUPABASE_ANON_KEY = "sb_publishable_T_AjtavafPYU-ciQ4q_Lfg_t08Xv47P"; // Safe for browser
+
+const  client= createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+
+// Example: Fetch data from a table named 'products'
+async function fetchNumberOfImages() {
+    const { data, error } = await client
+        .from('tomato_detections')
+        .select('*', { count: 'exact' }); // Get the exact count of rows
+
+    if (error) {
+        console.error("Error fetching data:", error);
+        document.getElementById('data-container').innerText = "Failed to load data.";
+        return;
+    }
+
+    // Render data dynamically into HTML
+    const statsContainer = document.getElementById('totalImages');
+    console.log("Fetched data:", data);
+    statsContainer.innerText = count; // Display total number of images processed
+}
+
+// Call the function on page load
+fetchNumberOfImages();
+
