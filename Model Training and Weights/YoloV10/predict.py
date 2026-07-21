bb61601bcfea1fb5c29c72e92b3e2ed37eea5c09
Oliver Goodrum <olivergoodrum.og@gmail.com>
2026-07-21 13:01:22 -0700

    Fix flake8 problems in gunicorn.conf.py and predict.py

--- Model Training and Weights/YoloV10/predict.py
+++ Model Training and Weights/YoloV10/predict.py
@@ -1,5 +1,4 @@
 import cv2
-import torch
 from ultralytics import YOLO
 
 # Load the YOLOv8 model (choose 'yolov8n.pt', 'yolov8s.pt', etc. for different sizes)
@@ -28,23 +27,23 @@ while video_capture.isOpened():
     ret, frame = video_capture.read()  # Read a frame
     if not ret:
         break
-    
+
     # Apply YOLOv8 object detection
     results = model(frame)[0]
-    
+
     # Iterate through the detections and draw bounding boxes
     for result in results.boxes.data.tolist():  # Each detection in the format [x1, y1, x2, y2, conf, class]
         x1, y1, x2, y2, conf, cls = result[:6]
         label = f'{model.names[cls]} {conf:.2f}'
-        
+
         # Draw bounding box and label on the frame
-        if conf > 0.5: 
-                cv2.rectangle(frame, (int(x1), int(y1)), (int(x2), int(y2)), (0, 0, 255), 4)  # Bounding box
-                cv2.putText(frame, label, (int(x1), int(y1) - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 2)
-    
+        if conf > 0.5:
+            cv2.rectangle(frame, (int(x1), int(y1)), (int(x2), int(y2)), (0, 0, 255), 4)  # Bounding box
+            cv2.putText(frame, label, (int(x1), int(y1) - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 2)
+
     # Write the processed frame to the output video
     out_video.write(frame)
-    
+
     # Print progress
     frame_count += 1
     print(f'Processed frame {frame_count}/{total_frames}')
@@ -53,4 +52,3 @@ while video_capture.isOpened():
 video_capture.release()
 out_video.release()
 print(f'Output video saved to {output_video_path}')
-
