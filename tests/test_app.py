bf4ebe830b0e8d22f3da5f4a49338ebfe06db8bb
Oliver Goodrum <olivergoodrum.og@gmail.com>
2026-07-21 16:21:39 -0700

    Created new tests for full test coverage of routes.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -4,6 +4,31 @@ def test_video_feed_route(client):
     assert response.status_code == 200
 
 def test_index_route(client):
-    """Test that the video feed route responds with success."""
+    """Test that the index route responds with success."""
     response = client.get("/index.html")
     assert response.status_code == 200
+
+def test_default_route(client):
+    """Test that the default route responds with success."""
+    response = client.get("/")
+    assert response.status_code == 200
+
+def test_historical_images_route(client):
+    """Test that the historical images route responds with success."""
+    response = client.get("historical_images.html")
+    assert response.status_code == 200
+
+def test_notifications_route(client):
+    """Test that the notifications route responds with success."""
+    response = client.get("notifications.html")
+    assert response.status_code == 200
+
+def test_statistics_route(client):
+    """Test that the statistics route responds with success."""
+    response = client.get("statistics.html")
+    assert response.status_code == 200
+
+def test_route_not_found(client):
+    """Test that the fake route responds with failure."""
+    response = client.get("fake_route.html")
+    assert response.status_code == 404
