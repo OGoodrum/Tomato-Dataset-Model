137f21d1977d158c4470122c5fa4b2633b1c7398
Oliver Goodrum <olivergoodrum.og@gmail.com>
2026-07-22 13:23:30 -0700

    Refactor test_app.py so that the tests are parametrized

--- tests/test_app.py
+++ tests/test_app.py
@@ -19,43 +19,17 @@ def client(app):
     with app.test_client() as client:
         yield client
 
-def test_video_feed_route(client):
-    """Test that the video feed route responds with success."""
-    response = client.get("/video_feed")
-    assert response.status_code == 200
+@pytest.mark.parametrize("route, status_code", [
+    ("/video_feed", 200),
+    ("/index.html", 200),
+    ("/", 200),
+    ("/historical_images.html", 200),
+    ("/notifications.html", 200),
+    ("/statistics.html", 200),
+    ("/fake_route.html", 404),
+])
 
-
-def test_index_route(client):
-    """Test that the index route responds with success."""
-    response = client.get("/index.html")
-    assert response.status_code == 200
-
-
-def test_default_route(client):
-    """Test that the default route responds with success."""
-    response = client.get("/")
-    assert response.status_code == 200
-
-
-def test_historical_images_route(client):
-    """Test that the historical images route responds with success."""
-    response = client.get("historical_images.html")
-    assert response.status_code == 200
-
-
-def test_notifications_route(client):
-    """Test that the notifications route responds with success."""
-    response = client.get("notifications.html")
-    assert response.status_code == 200
-
-
-def test_statistics_route(client):
-    """Test that the statistics route responds with success."""
-    response = client.get("statistics.html")
-    assert response.status_code == 200
-
-
-def test_route_not_found(client):
-    """Test that the fake route responds with failure."""
-    response = client.get("fake_route.html")
-    assert response.status_code == 404
+def test_video_feed_route(client, route, status_code):
+    """Test that the routes respond with the correct status code."""
+    response = client.get(route)
+    assert response.status_code == status_code
