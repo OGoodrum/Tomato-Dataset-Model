ff309360dfeace3c72a494b87513cde8460dbd96
Oliver Goodrum <olivergoodrum.og@gmail.com>
2026-07-22 13:09:56 -0700

    All of the flask endpoints that render html can only be accessed with GET now

--- src/routes.py
+++ src/routes.py
@@ -5,32 +5,32 @@ from src.services.camera import generate_frames
 bp = Blueprint("main", __name__)
 
 
-@bp.route("/")
-@bp.route("/index.html")
+@bp.route("/", methods=['GET'])
+@bp.route("/index.html", methods=['GET'])
 def index():
     """Video streaming home page."""
     return render_template("index.html")
 
 
-@bp.route("/historical_images.html")
+@bp.route("/historical_images.html", methods=['GET'])
 def historical_images():
     """Historical images page."""
     return render_template("historical_images.html")
 
 
-@bp.route("/notifications.html")
+@bp.route("/notifications.html", methods=['GET'])
 def live_videos():
     """Notifications page."""
-    return render_template("notifications.html")
+    return render_template("notifications.html", methods=["GET"])
 
 
-@bp.route("/statistics.html")
+@bp.route("/statistics.html", methods=['GET'])
 def statistics():
     """Statistics page."""
     return render_template("statistics.html")
 
 
-@bp.route("/video_feed")
+@bp.route("/video_feed", methods=['GET'])
 def video_feed():
     """Video streaming route. Put this in the src attribute of an img tag."""
     return Response(
