41d1c550f33d14d4b004039f02f67d9bdd92b839
Oliver Goodrum <olivergoodrum.og@gmail.com>
2026-07-24 11:30:59 -0700

    Add login popup on all pages however there is no login functionality yet

---
+++ src/static/login.js
@@ -0,0 +1,25 @@
+// Get the modal
+var modal = document.getElementById('login_popup');
+
+
+const load_login = () => {
+    fetch(`../templates/login.html`)
+    .then(res => {
+        if (res.ok){
+            return res.text()
+        }
+    }).then(htmlSnippet => {
+        modal.innerHTML = htmlSnippet;
+    });
+};
+
+load_login();
+
+
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
