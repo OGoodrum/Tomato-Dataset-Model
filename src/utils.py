dcf687c63d9b8da337ee31688e79031076ee9c0b
Oliver Goodrum <olivergoodrum.og@gmail.com>
2026-07-27 16:05:36 -0700

    login now works, but it is prompted for every html page instead of being saved

--- src/utils.py
+++ src/utils.py
@@ -2,11 +2,30 @@ from functools import wraps
 
 from flask import make_response, request, current_app, render_template
 
+from src.services.database import get_supabase_client
+
 def auth_required(f):
+
+
     @wraps(f)
     def decorated(*args, **kwargs):
+
         auth = request.authorization
-        if auth and auth.username == current_app.config.get("SITE_USER") and auth.password == current_app.config.get("SITE_PASS"):
+
+        db_client = get_supabase_client()
+        response = None
+
+        try:
+            if auth and auth.username and auth.password:
+                response = db_client.table("users").select('username, password').eq("username", auth.username).execute()
+                print(response)
+        except Exception as e:
+            print(f"[DB] Error querying Supabase: {e}")
+            return make_response(f"<h1>Database error {e}</h1>", 500)
+
+        
+            
+        if auth and response and len(response.data) > 0 and response.data[0]["password"] == auth.password:
             return f(*args, **kwargs)
         response = make_response(render_template("/access_denied.html"), 401)
         response.headers['WWW-Authenticate'] = 'Basic realm="Login required!"'
