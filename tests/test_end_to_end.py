5f3fcacd6b046f833a452ce06ed197657c896266
Oliver Goodrum <olivergoodrum.og@gmail.com>
2026-07-30 14:31:08 -0700

    refactor end to end tests

--- tests/test_end_to_end.py
+++ tests/test_end_to_end.py
@@ -1,29 +1,22 @@
 import re
 import pytest
-from playwright.sync_api import Page, expect, Playwright
-
+from playwright.sync_api import Page, expect
 
 @pytest.fixture(scope="function", autouse=True)
-def before_each(page: Page):
+def goto_localhost_5000(page: Page):
     page.goto("http://localhost:5000")
-    yield
 
 def test_has_title(page: Page):
-
+    
     expect(page).to_have_title(re.compile("Tomato Detection Stream"))
 
 def test_redirect(page: Page):
     expect(page).to_have_url("http://localhost:5000/login.html")
 
-def test_user_login(playwright: Playwright) -> None:
-    browser = playwright.chromium.launch(headless=True)
-    context = browser.new_context()
-    page = context.new_page()
-    page.goto("http://localhost:5000/login.html")
+def test_user_login(page: Page) -> None:
     page.get_by_role("textbox", name="Enter Username").click()
     page.get_by_role("textbox", name="Enter Username").fill("user")
     page.get_by_role("textbox", name="Enter Password").click()
     page.get_by_role("textbox", name="Enter Password").fill("pass")
     page.get_by_role("button", name="Login").click()
-
      
